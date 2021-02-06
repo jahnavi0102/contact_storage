0beb41d482f8eff4de9d375eac1575c755dd6eca
jahnavi0102 <jjain.rjc@gmail.com>
2021-02-06 16:21:30 +0530

    models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, ContactsInfo
+
+admin.site.register(User)
+admin.site.register(ContactsInfo)
 
 # Register your models here.
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class User(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    user_name = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ContactsInfo(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    full_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20, unique=True)
+    dob = models.DateField(max_length=8)
+    gmail = models.EmailField(max_length = 254) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
